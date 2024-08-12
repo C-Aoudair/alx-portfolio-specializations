5820c1a6b94fb8c5f7d65574edffccacf369c5ac
C-Aoudair <aoudair16031999@gmail.com>
2024-08-12 15:51:43 +0100

    setup the project environment

--- SkillSwap/backend/SkillSwap/SkillSwap/settings.py
+++ SkillSwap/backend/SkillSwap/SkillSwap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9(fjpao%v!g=eg3ly#lp!8hk5uobe=l*+!v3_ey34by*m(^o7c"
+SECRET_KEY = "django-insecure-%jkd4xmzkehq@gr%dt)bnulbzypm=mu9v8m6-k%^3nkijt@8^!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
